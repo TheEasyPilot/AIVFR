5cee00bfe7d4808332d7cc3fdf371e4a168eabde
AlimMugalaasi <abdulalimkasirye@gmail.com>
2025-09-08 22:17:48 +0000

    commenting toggle

--- AIVFR/static/settings.js
+++ AIVFR/static/settings.js
@@ -1,11 +1,12 @@
-const root = document.documentElement
+//-----------light/dark toggle----------------
+const root = document.documentElement //this is the <html> element in settngs.html
 const lightMode = document.getElementById("lightToggle")
 const darkMode = document.getElementById("darkToggle")
 
 darkMode.addEventListener("click", () => {
     root.classList.add("dark-mode");
-});
+});//adding the class of the darkmode colours to the html page, so it applies those rules instead
 
 lightMode.addEventListener("click", () => {
     root.classList.remove("dark-mode");
-});
+});//reversing it so that it just applies whatever is in the :root
