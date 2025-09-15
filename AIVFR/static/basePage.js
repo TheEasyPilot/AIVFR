63658252a9f4067dbb082dd2ad5802cee470991b
AlimMugalaasi <abdulalimkasirye@gmail.com>
2025-09-15 15:56:45 +0000

    navigation complete

--- AIVFR/static/basePage.js
+++ AIVFR/static/basePage.js
@@ -15,8 +15,8 @@ function updateClock() { //function allows just the time to update smoothly
 updateClock() //when page loads for first time, run it immediately...
 setInterval(updateClock, 1000); //...then update every second.
 
-//Hovering over a tab when selecting---------------------
-
+//----------------------Hovering over a tab when selecting---------------------
+//pair each button to their label (as I didn't code them inline with eachother)
 const pairs = {
   dashboard: "dashboardLABEL",
   route: "routeLABEL",
@@ -28,15 +28,19 @@ const pairs = {
 };
 
 for (let buttonId in pairs) {
-  const button = document.getElementById(buttonId);
-  const LABEL = document.getElementById(pairs[buttonId]);
+  const button = document.getElementById(buttonId);//assign each button with button variable
+  const LABEL = document.getElementById(pairs[buttonId]); //assign eachl label with LABEL variable
+  const gradient = document.getElementById("NAVoverlay");
 
-  if (button && LABEL) {
+  if (button && LABEL) { //For each matching pair run an event listner for it
     button.addEventListener("mouseenter", () => {
       LABEL.style.display = "block";
+      gradient.style.display = "block";
     });
     button.addEventListener("mouseleave", () => {
       LABEL.style.display = "none";
+      gradient.style.display = "none";
     });
   }
 }
+
