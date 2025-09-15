db18ed6cc740ac744b3c058f28f70fde34de3029
AlimMugalaasi <abdulalimkasirye@gmail.com>
2025-09-15 11:29:08 +0000

    navwork agian

--- AIVFR/static/basePage.js
+++ AIVFR/static/basePage.js
@@ -15,3 +15,28 @@ function updateClock() { //function allows just the time to update smoothly
 updateClock() //when page loads for first time, run it immediately...
 setInterval(updateClock, 1000); //...then update every second.
 
+//Hovering over a tab when selecting---------------------
+
+const pairs = {
+  dashboard: "dashboardLABEL",
+  route: "routeLABEL",
+  weather: "weatherLABEL",
+  navlog: "navLABEL",
+  fuel: "fuelLABEL",
+  "m&b": "mass_and_balanceLABEL",
+  performance: "performanceLABEL"
+};
+
+for (let buttonId in pairs) {
+  const button = document.getElementById(buttonId);
+  const LABEL = document.getElementById(pairs[buttonId]);
+
+  if (button && LABEL) {
+    button.addEventListener("mouseenter", () => {
+      LABEL.style.display = "block";
+    });
+    button.addEventListener("mouseleave", () => {
+      LABEL.style.display = "none";
+    });
+  }
+}
