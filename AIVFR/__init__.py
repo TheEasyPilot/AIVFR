024c2068db98e2d95c1483c8711c1c73d1aa3e33
AlimMugalaasi <abdulalimkasirye@gmail.com>
2025-09-18 12:36:16 +0000

    JSON work

--- AIVFR/__init__.py
+++ AIVFR/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
+import os
+from dotenv import load_dotenv
 
 def create_app():
     app = Flask(__name__)
+    app.secret_key = os.getenv("SESSION_KEY")
 
     from .app import main as app_blueprint
     app.register_blueprint(app_blueprint)
     
-
     return app
