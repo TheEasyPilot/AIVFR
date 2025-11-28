baa933b723971dbe06b1f732dd53a14593d36de3
ItsTheEasyPilot <theeasypilot@yahoo.com>
2025-11-28 14:38:38 +0000

    intial layout complete

--- AIVFR/static/weather.js
+++ AIVFR/static/weather.js
@@ -1,1 +1,7 @@
 import { update, showAlert } from "./basePage.js";
+
+const update_wx = document.getElementById('update')
+
+update_wx.addEventListener('click', function() {
+    showAlert('test');
+})
